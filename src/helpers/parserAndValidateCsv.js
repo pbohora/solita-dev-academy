3acb1b7460203fcf225c18b17f29ee4d4efbae40
@pradipbohora <pradip.bohora@talbit.fi>
2022-01-12 23:23:45 +0200

    fix lint

--- src/helpers/parserAndValidateCsv.js
+++ src/helpers/parserAndValidateCsv.js
@@ -1,5 +1,5 @@
-const csv = require("fast-csv");
-const csvValidator = require("./csvValidator");
+const csv = require('fast-csv');
+const csvValidator = require('./csvValidator');
 
 const parserAndValidateCsv = (file) => {
   return new Promise((resolve, reject) => {
@@ -7,21 +7,21 @@ const parserAndValidateCsv = (file) => {
     // open uploaded file
     csv
       .parseFile(file, { headers: true })
-      .on("data", function (data) {
+      .on('data', function (data) {
         if (
-          !Object.prototype.hasOwnProperty.call(data, "location") ||
-          !Object.prototype.hasOwnProperty.call(data, "datetime") ||
-          !Object.prototype.hasOwnProperty.call(data, "sensorType") ||
-          !Object.prototype.hasOwnProperty.call(data, "value")
+          !Object.prototype.hasOwnProperty.call(data, 'location') ||
+          !Object.prototype.hasOwnProperty.call(data, 'datetime') ||
+          !Object.prototype.hasOwnProperty.call(data, 'sensorType') ||
+          !Object.prototype.hasOwnProperty.call(data, 'value')
         ) {
-          return reject({ error: "missing field" });
+          return reject({ error: 'missing field' });
         }
         const isDataValid = csvValidator(data);
         if (isDataValid) {
           fileRows.push(data);
         }
       })
-      .on("end", function () {
+      .on('end', function () {
         fileRows && fileRows.length > 0
           ? resolve({ farm: fileRows[0].location, data: fileRows })
           : reject();

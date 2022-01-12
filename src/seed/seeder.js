02163ebb7a69d2927908cb46000a7d1d42f5bc7a
@pradipbohora <pradip.bohora@talbit.fi>
2022-01-12 17:52:35 +0200

    get all files

--- src/seed/seeder.js
+++ src/seed/seeder.js
@@ -2,8 +2,28 @@ const path = require("path");
 const parserAndValidateCsv = require("../helpers/parserAndValidateCsv");
 const Survey = require("../models/survey");
 
-const seedSurveys = parserAndValidateCsv(
-  path.resolve(__dirname, "../csv_files", "partialTech.csv")
-)
+const partialTechCsv = path.resolve(
+  __dirname,
+  "../csv_files",
+  "PartialTech.csv"
+);
+
+const frimanMetsolaCsv = path.resolve(
+  __dirname,
+  "../csv_files",
+  "friman_metsola.csv"
+);
+
+const ossiFarmCsv = path.resolve(__dirname, "../csv_files", "ossi_farm.csv");
+
+const NoorasFarmCsv = path.resolve(
+  __dirname,
+  "../csv_files",
+  "Nooras_farm.csv"
+);
+
+console.log(partialTechCsv);
+
+const seedSurveys = parserAndValidateCsv(partialTechCsv)
   .then((data) => console.log(data))
   .catch((err) => console.log(err));

ee39b8ff727cba6abdcca2b8df3289ff722092af
@pradipbohora <pradip.bohora@talbit.fi>
2022-01-13 22:27:23 +0200

    fix initial test data load bug

--- src/tests/testHelpers/helpers.js
+++ src/tests/testHelpers/helpers.js
@@ -1,41 +1,45 @@
-const Farm = require('../../models/farm');
-const Survey = require('../../models/survey');
+const Farm = require("../../models/farm");
+const Survey = require("../../models/survey");
 
 const surveys = [
   {
     value: 5.9,
-    date: '2018-12-31T22:00:00.000+00:00',
-    sensorType: 'pH',
+    date: "2018-12-31T22:00:00.000+00:00",
+    sensorType: "pH",
   },
   {
     value: 80,
-    date: '2019-12-31T22:00:00.000+00:00',
-    sensorType: 'temperature',
+    date: "2019-12-31T22:00:00.000+00:00",
+    sensorType: "temperature",
   },
   {
     value: 60,
-    date: '2020-12-31T22:00:00.000+00:00',
-    sensorType: 'rainFall',
+    date: "2020-12-31T22:00:00.000+00:00",
+    sensorType: "rainFall",
   },
 ];
-const farmData = { name: 'test farm' };
+const farmData = { name: "test farm" };
 
 const initialDataLoad = async () => {
   await Survey.deleteMany({});
   await Farm.deleteMany({});
-  const farm = new Farm(farmData);
-  await farm.save();
-  const savedFarm = farm.toJSON();
+  const newFarm = new Farm(farmData);
+  await newFarm.save();
+
+  const farm = newFarm.toJSON();
 
   const modifiedSurveys = surveys.map((survey) => ({
     ...survey,
-    farm: savedFarm.id,
+    farm: farm.id,
   }));
 
   const savedSurvays = await Survey.insertMany(modifiedSurveys);
-  farm.surveys = [...farm.surveys, ...savedSurvays.map((survey) => survey._id)];
+  newFarm.surveys = [
+    ...newFarm.surveys,
+    ...savedSurvays.map((survey) => survey._id),
+  ];
 
-  await farm.save();
+  await newFarm.save();
 };
 
 module.exports = { initialDataLoad };
